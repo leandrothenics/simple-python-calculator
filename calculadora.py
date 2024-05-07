0d693d88c4e472692d4e4e758f3a714f6e5b5e5c
leandrothenics <168592566+leandrothenics@users.noreply.github.com>
2024-05-07 20:26:54 -0300

    Delete calculadora.py

--- calculadora.py
+++
@@ -1,8 +0,0 @@
-def soma(a, b):
-    return a + b 
-
-if __ame__=="_main_":
-    num1 = float(input("Digite o primeiro numero: "))
-    num2 = float(inmpu("Digite o segundo numero: "))
-    resultado = soma(num1, num2) 
-    print(f"Resultado: {resultado}") 
